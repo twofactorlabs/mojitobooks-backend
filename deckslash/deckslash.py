d955d1f21254c6cd4589266b1553b0e0ab61a36d
Duy Tung <duytungha.dev@gmail.com>
2018-11-07 21:00:08 +0700

    update register logic

--- deckslash/deckslash.py
+++ deckslash/deckslash.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -9,9 +9,12 @@ app.config['SECRET_KEY'] = 'c24170e00d0368506c875c08b016f5b9'
 def home() -> 'html':
     return render_template('index.html', title='Welcome')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
